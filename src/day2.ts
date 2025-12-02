92a0657ac98115cb7e62c42ebeb08f4424817f45
Zoë Mackay <zoe.mackay@ovoenergy.com>
2025-12-02 10:23:41 +0000

    day2 refactor

--- src/day2.ts
+++ src/day2.ts
@@ -13,42 +13,43 @@ const parseRange = (range: string): Range => {
   throw new Error('Invalid range')
 }
 
-export const day2pt1 = (stringRanges: string[]): number => {
+const day2Process = (
+  stringRanges: string[],
+  matchFn: (stringN: string) => boolean
+): number => {
   let total = 0
   const ranges = stringRanges.map(parseRange)
   ranges.forEach((range) => {
     for (let i = range.from; i <= range.to; i++) {
-      const stringI = i.toString()
-      const stringILength = stringI.length
-      if (stringILength % 2 == 0) {
-        if (
-          stringI.substring(0, stringILength / 2) ==
-          stringI.substring(stringILength / 2)
-        ) {
-          total += i
-        }
+      if (matchFn(i.toString())) {
+        total += i
       }
     }
   })
   return total
 }
 
+export const day2pt1 = (stringRanges: string[]): number => {
+  return day2Process(stringRanges, (stringN) => {
+    const stringNLength = stringN.length
+    return (
+      stringNLength % 2 == 0 &&
+      stringN.substring(0, stringNLength / 2) ==
+        stringN.substring(stringNLength / 2)
+    )
+  })
+}
+
 export const day2pt2 = (stringRanges: string[]): number => {
-  let total = 0
-  const ranges = stringRanges.map(parseRange)
-  ranges.forEach((range) => {
-    for (let i = range.from; i <= range.to; i++) {
-      const stringI = i.toString()
-      const stringILength = stringI.length
-      for (let j = 1; j <= stringILength / 2; j++) {
-        const chunked = chunkString(stringI, j)
-        const chunkedSet = new Set(chunked)
-        if (chunkedSet.size == 1) {
-          total += i
-          break
-        }
+  return day2Process(stringRanges, (stringN) => {
+    const stringNLength = stringN.length
+    for (let j = 1; j <= stringNLength / 2; j++) {
+      const chunked = chunkString(stringN, j)
+      const chunkedSet = new Set(chunked)
+      if (chunkedSet.size == 1) {
+        return true
       }
     }
+    return false
   })
-  return total
 }
